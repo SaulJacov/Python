8691b46549e0f6f29003de4af7b7c69d8a23d873
SaulJacov <smartinezj1700@alumno.ipn.mx>
2025-01-30 00:12:32 -0600

    listo
    listp

---
+++ hola_mundo.py
@@ -0,0 +1,7 @@
+#hola como estas perro
+print('hola Mundo')
+
+#tipos de datos
+print(type('esto es texto'))
+print(type(5))
+print(type(1.5))
---
+++ variables.py
@@ -0,0 +1,16 @@
+#variables
+
+a = 4 
+b = 6
+c = a + b
+print(c)
+
+mi_texto = "texto"
+
+print(type(mi_texto))
+
+mi_numero = 11
+
+print(mi_texto,mi_numero)
+
+print('mi numero favorto es:', mi_numero)
