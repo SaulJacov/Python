7a3dfd50b6f762ebcf373af8c50a8a56e53cb198
SaulJacov <martinezjimenezsauljacob@gmail.com>
2025-01-31 09:19:31 -0600

    Update hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -5,5 +5,9 @@ print("hola esta es una cadena que se mostrara en la consola")
 
 #tipos de datos
 print('esto es texto')
-print(5)
+print(5) 
 print(1.5)
+
+# esta es una cadena con formato
+age = 22
+print(f"la edad de Jacob es {age}")
